c58b7760ba07ddc44934a47aa5482800fa48fe6d
jaisonalievi <jaison.alieve@gmail.com>
2022-11-30 13:58:37 -0300

    Added new entity named company test_

--- src/main/java/com/example/demo/crudYoutube/api/person/PersonFakeDaoRepository.java
+++ src/main/java/com/example/demo/crudYoutube/api/person/PersonFakeDaoRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
-public class FakePersonDataAccessService implements PersonDao {
+public class PersonFakeDaoRepository implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
 
